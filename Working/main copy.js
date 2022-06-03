97e3483b45d1d8e9062c77a584d3cdece78157ea
Craig Edwards <jamesallyncraigedwards@gmail.com>
2022-06-03 18:21:56 -0400

    First

---
+++ Working/main copy.js
@@ -0,0 +1,52 @@
+console.log('test')
+
+// Client Id: c2664ec3b0c04d188b08620542866fae
+// Secret Key: 78763df6e57c44d08a07ae821cfbe8a0
+
+const getAuth = async () => {
+    const clientID = 'c2664ec3b0c04d188b08620542866fae'
+    const clientSecret = '78763df6e57c44d08a07ae821cfbe8a0'
+    const encodedString = btoa(clientID + ":" + clientSecret)
+    const response = await fetch('https://accounts.spotify.com/api/token',
+    {
+        method: 'POST',
+        headers: {
+            'Authorization': `Basic ${encodedString}`,
+            'Content-Type' : 'application/x-www-form-urlencoded'
+        },
+        body: 'grant_type=client_credentials'
+    }
+    );
+    let token = await response.json()
+    console.log(token)
+    return token.access_token
+}
+
+const loadToken = async () => {
+    const token = await getAuth();
+    console.log(token);
+    return token
+}
+
+// returned_value = loadToken();
+// console.log(returned_value)
+
+const getSong = async () =>{
+    const token = await loadToken();
+    let data = await fetch(`https://api.spotify.com/v1/search?type=track&q=track:anemone+artist:slenderbodies&limit=1`,
+        {
+            method: 'GET',
+            headers: {
+                'Content-Type' : 'application/json',
+                'Authorization' : `Bearer ${token}`
+            }
+        });
+    data = await data.json();
+    console.log(data)
+    console.log(data.tracks.items[0].preview_url);
+    let audioobj = new Audio(data.tracks.items[0].preview_url);
+    audioobj.play();
+}
+
+let playbutton = document.querySelector('#pbtn')
+playbutton.addEventListener('click', () => {getSong();});
